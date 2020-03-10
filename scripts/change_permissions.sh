31b49e653927765791cab6807386982b5833b002
JonathonMurphy <jsmurphy1991@gmail.com>
2020-03-10 13:26:54 -0400

    Updating deployment scripts

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-chown ubuntu:ubuntu  -R /var/www/mbmbam.app/*; chown ubuntu:ubuntu  -R /var/www/mbmbam.app/.*
+chown ubuntu:ubuntu  -R /var/www/mbmbam.app/* && chown ubuntu:ubuntu  -R /var/www/mbmbam.app/.*
