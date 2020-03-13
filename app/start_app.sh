297a3735e0fb647509f8e2e6205f4e80eb7c4584
JonathonMurphy <jsmurphy1991@gmail.com>
2020-03-13 08:44:40 -0400

    Adding more redirects in start script

--- app/start_app.sh
+++ app/start_app.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$NODE_ENV" = "production" ]; then
-  node server.js > app.out.log 2> app.err.log & 
+  node server.js > app.out.log 2> app.err.log < /dev/null & 
 else
   nodemon --ignore './sessions' server.js;
 fi
